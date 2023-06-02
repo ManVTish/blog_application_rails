824db664cf80fb89aa818f5215ac84f063424184
Manish Roy <manishroy0112@gmail.com>
2023-06-02 14:18:22 +0530

    create endpoint added for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: {msg: "Post creation successful"}
+    else
+      render json: {msg: "Post creation failed"}
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:posts).permit(:title, :body)
   end
 end
