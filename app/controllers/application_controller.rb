c009726764835afce4a9516195d0b9a4939d6550
Manish Roy <manishroy0112@gmail.com>
2023-06-09 10:55:45 +0530

    updated to resolve AA & pundit conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   before_action :authenticate_user!
+
+  # after_action :verify_authorized, except: :index, unless: :active_admin_controller?
+  # after_action :verify_policy_scoped, only: :index, unless: :active_admin_controller?
+
+  # def active_admin_controller?
+  #   is_a?(ActiveAdmin::BaseController)
+  # end
 end
